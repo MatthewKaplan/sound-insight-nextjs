5f6a5a5f30caf9442be9d691c9579ed134bd5ac2
Matthew Kaplan <mkaplan@netsolace.com>
2021-12-30 17:58:23 -0500

    fix headers

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
-import headers from './helper/headers';
-
+/* eslint-disable require-await */
+/* eslint-disable @typescript-eslint/require-await */
 module.exports = {
   images: {
     domains: ['i.imgur.com', 'res.cloudinary.com']
@@ -8,12 +8,42 @@ module.exports = {
     firebasedb_link: 'https://sound-insight-default-rtdb.firebaseio.com/PAGE_DATA.json',
     google_analytics: 'UA-148182336-1'
   },
-  headers() {
-    return [
+  async headers() {
+    return [{
+      source: '/about',
+      headers: [{
+        key: 'X-DNS-Prefetch-Control',
+        value: 'on'
+      },
       {
-        source: '/(.*)',
-        headers
+        key: 'Strict-Transport-Security',
+        value: 'max-age=63072000; includeSubDomains; preload'
+      },
+      {
+        key: 'Server',
+        value: 'Apache'
+      },
+      {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff'
+      },
+      {
+        key: 'X-Frame-Options',
+        value: 'sameorigin'
+      },
+      {
+        key: 'X-XSS-Protection',
+        value: '1; mode=block'
+      },
+      {
+        key: 'Referrer-Policy',
+        value: 'same-origin'
+      },
+      {
+        key: 'Permissions-Policy',
+        value: 'geolocation=*'
       }
-    ];
+      ]
+    }];
   }
 };
