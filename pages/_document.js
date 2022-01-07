01bc3def3e773f744a40d0e222363bebe9978c1b
Matthew Kaplan <mkaplan@netsolace.com>
2022-01-07 09:42:17 -0500

    Head issue fix?

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import Document, {
-  Html, Main, NextScript
+  Html, Main, NextScript, Head
 } from 'next/document';
 
 class MyDocument extends Document {
@@ -11,6 +11,21 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-148182336-1" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-148182336-1', {
+              page_path: window.location.pathname,
+            });
+          `
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
